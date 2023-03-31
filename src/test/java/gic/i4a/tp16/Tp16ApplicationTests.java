e26bb293c4fe88afd6028bb7cc7ade20dd367c5e
KHEANG-LYMENG <lymengkheang1307@gmail.com>
2023-03-31 08:52:49 +0700

    TP16-SoftwareEngineering * Done Task1 * Done Task2 * Done Task3 * Done Task4 * Ongoing Task5

---
+++ src/test/java/gic/i4a/tp16/Tp16ApplicationTests.java
@@ -0,0 +1,13 @@
+package gic.i4a.tp16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
